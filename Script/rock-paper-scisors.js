d17ffdbd4a0054f0bc88e0e38b9d103263d0b32a
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-03-31 13:04:58 +0100

    Add files via upload

--- Script/rock-paper-scisors.js
+++ Script/rock-paper-scisors.js
@@ -52,7 +52,10 @@ function playGame(playerMove) {
 
   document.querySelector('.js-result').innerHTML = result;
 
-  document.querySelector('.js-moves').innerHTML = `You picked ${playerMove} Computer picked ${computerMove}`;
+  document.querySelector('.js-moves').innerHTML = `You 
+  <img src="Icons/${playerMove}-emoji.png" class="move-icon">
+  <img src="Icons/${computerMove}-emoji.png" class="move-icon">
+  Computer`;
 }
 
 function updateScoreElement() {
